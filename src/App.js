d5089df53481f4fed1a07c0062f89db60a38d20d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-07 16:22:25 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
-// import Values from 'values.js'
+import Values from 'values.js'
 
 function App() {
+  const [color, setColor] = useState('')
   return <h2>color generator project</h2>
 }
 
