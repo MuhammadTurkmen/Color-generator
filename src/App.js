7a2e9005de871466f90471a43bec23781b8ce97b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-07 16:44:28 +0430

    init

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [list, setList] = useState([])
 
   const handleSubmit = (e) => { 
-    e.preventDefault()
+    e.preventDefault() 
     try {
       
     } catch (error) {
