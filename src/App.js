a0572349f06bdaf1db6150bbc7567d210a9f9530
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-07 16:25:45 +0430

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState()
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+
+  }
+  
   return <h2>color generator project</h2>
 }
 
