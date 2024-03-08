5832ca47d822dc6d6119c42da2f92bdd178e606f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 09:20:28 +0430

    init

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,7 +4,9 @@ import rgbToHex from './utils'
 const SingleColor = ({rgb, weight, index}) => {
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
-  return <article className={`color`} style={{backgroundColor:`rbg(${bcg})`}}></article>
+  return <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
+    
+  </article>
 }
 
 export default SingleColor
