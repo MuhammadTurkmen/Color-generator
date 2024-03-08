89f20bf81930f6d4f0a5e7ca96f5e3d2479ca51e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 09:32:33 +0430

    init

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,7 +5,7 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
   const hex = rgbToHex(...rgb)
-  return <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
+  return <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}>
     <p className='percent-value'>
       {weight}%
     </p>
