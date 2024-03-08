10615046efb42e37f4026425feaf18d0c4a118d9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 16:59:08 +0430

    init

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
     </section>
     <section className="colors">
       {list.map((color, index) => {
-        return
+        return <SingleColor />
       })}
     </section>
     </>
