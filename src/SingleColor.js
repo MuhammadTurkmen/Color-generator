bd546c4f6a7cb3b5661036eca936ad2deb6aff7f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 09:46:19 +0430

    init

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,7 +7,10 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
   useEffect(() => {
-
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
   }, [alert])
   
   return <article 
