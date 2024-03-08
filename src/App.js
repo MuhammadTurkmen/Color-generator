8b633af7de654161ff726fc4b1225a66d6724e8f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 16:57:05 +0430

    init

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     e.preventDefault() 
     try { 
       let colors = new Values(color).all(10) 
-      console.log(colors);
+      setList(colors)
     } catch (error) {
       setError(true)
       console.log(error);
