a5e5d2d55b660b94ad0217577d0b742529769355
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 09:10:36 +0430

    init

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -3,6 +3,7 @@ import rgbToHex from './utils'
 
 const SingleColor = ({rgb, weight, index}) => {
   const [alert, setAlert] = useState(false)
+  const bcg = rgb.join(',')
   return <h4>single color</h4>
 }
 
