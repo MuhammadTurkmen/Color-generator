0a00773c6db7376b302c959fe4772cf8cce74aa0
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 09:25:54 +0430

    init

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,11 +4,12 @@ import rgbToHex from './utils'
 const SingleColor = ({rgb, weight, index, hex}) => {
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
+  const hex = 
   return <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
     <p className='percent-value'>
       {weight}%
     </p>
-    <p className='color-value'>{hex}</p>
+    <p className='color-value'></p>
   </article>
 }
 
