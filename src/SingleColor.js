02472a1d593d627d81b8953ce71b901a403a0ff6
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-08 09:24:53 +0430

    init

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({rgb, weight, index}) => {
+const SingleColor = ({rgb, weight, index, hex}) => {
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
   return <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
     <p className='percent-value'>
       {weight}%
     </p>
-    <p className='color-value'></p>
+    <p className='color-value'>{hex}</p>
   </article>
 }
 
